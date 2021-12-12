6ee3d0af6806384d04527e659324dcae3f85b43d
Ayaan R <53174223+astronomerayaan@users.noreply.github.com>
2021-12-12 00:23:05 -0800

    keypress
    added keypress which will show image and fixed 1 formatting issue

--- main.js
+++ main.js
@@ -29,30 +29,35 @@ function my_keydown(e)
 keyPressed = e.keyCode;
 console.log(keyPressed);
 
-	if(keyPressed == '?') // add appropriate keycode
+	if(keyPressed == '82') 
 	{
-		// upload red ranger
+		new_image('rr1.png');
+		console.log("r");
 	}
 	if(keyPressed == '71')
 	{
 		block_x = 200;
-		// upload green ranger
+		new_image('gr.png');
+		console.log("g");
 	}
 	
 	if(keyPressed == '89')
 	{
-		block_x =350;
-		// upload yellow ranger
+		block_x = 350;
+		new_image('yr.png');
+		console.log("y");
 	}
 	if(keyPressed == '80')
 	{
 		block_x = 600;
-		// upload pink ranger
+		new_image('pr.png');
+		console.log("p");
 	}
 	if(keyPressed == '66')
 	{
 		block_x = 700;
-	// upload blue ranger
+		new_image('br.png');
+		console.log("g");
 	}
 	
 }
